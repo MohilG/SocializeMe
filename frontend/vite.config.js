27717bad8e640d5658ef0864df973b456c9a1f28
MohilG <mohilg3703@gmail.com>
2024-04-20 20:47:59 +0530

    Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   build: { manifest: true, outDir: './dist'},
   base:  '/',
-  root: './src',
+  // root: './src',
   plugins: [react()],
   server:{
     port:3000,
