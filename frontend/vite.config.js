13bfb57fc06ae72b5b3c24949b4152b5f7cbefb2
MohilG <102367799+MohilG@users.noreply.github.com>
2024-04-20 20:48:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { manifest: true, outDir: './dist'},
-  base:  '/',
-  root: './src',
+  // base:  '/',
+  // root: './src',
   plugins: [react()],
   server:{
     port:3000,
