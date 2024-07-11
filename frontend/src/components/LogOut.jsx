df4ef5488235c177fb3cb8e0c830b3635957092a
MohilG <102367799+MohilG@users.noreply.github.com>
2024-07-11 15:05:26 +0530

    Update LogOut.jsx

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -39,7 +39,7 @@ const LogOut = () => {
             }
     }
   return (
-    <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"} onClick={handleLogOut}>LogOut</Button>
+    <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"} onClick={()=>handleLogOut}>LogOut</Button>
   )
 }
 
